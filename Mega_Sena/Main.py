fc330f04784b85ee5a9fa51f8702877140e473f6
jhonatan <jhonatansantos070@gmail.com>
2022-07-16 10:58:37 -0300

    adicionando os riscos na cartela

--- Mega_Sena/Main.py
+++ Mega_Sena/Main.py
@@ -1,21 +1,51 @@
-
+#import das bibliotecas necessarias
 import random
+from tokenize import Ignore
+#cria a matriz para ser usada na função
 matriz_s = []
 
 
-def mostra_cartela(numeros[]):
-#for menor in range(0, 6):
+def mostra_cartela(numeros):
+#for que gera os numeros da cartela
     for maior in range(0, 60):
-                
+        
         matriz_s.append(maior + 1)
+    #variavel para verificar o tamanho da cartela
     cartela = 0
+    #variavel para verificar a quantidade de numeros marcados
+    cartela_marcado = 0
+    
     while cartela < 60:
+        #if para verificar se a quantidade de numeros marcados foi excedida
+        if len(numeros) > cartela_marcado:
+            Ignore
+            #if que verificar quak numero foi sorteado para colocar na cartela
+            if(matriz_s[cartela] == numeros[cartela_marcado]):
+                print("-", end= "")
+                cartela_marcado += 1
+                cartela -= 1
+        # print("cartela ", cartela, end=" ")
         print(matriz_s[cartela], end=' ')
         cartela += 1
-        
+        #if para pular linhas de 10 em 10
         if(cartela == 10 or cartela == 20 or cartela == 30 or cartela == 40 or cartela == 50 or cartela == 60):
             print("\n")
-            
+    print('tamanho ', len(matriz_s))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("Quantos numeros você deseja jogar: 4, 5 ou 6 numeros ", end="")
 jogo = int(input())
 if(jogo >= 4) and (jogo <= 6):
@@ -23,6 +53,11 @@ if(jogo >= 4) and (jogo <= 6):
     for i in range(0, jogo):
         gera = random.randint(1, 60)
         numeros.append(gera)
+    
+    numeros.sort()
+    print("numeros: ", numeros)
+    mostra_cartela(numeros)
+    
     
     
     
